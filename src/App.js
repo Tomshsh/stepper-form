9eabd2d61dd47845b5932ff5f8408cba12b7bb56
Tomer <shmtomer@gmail.com>
2021-05-31 17:51:07 +0300

    login page styling

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import Form from './app/pages/Form/Form';
 
 let theme = createMuiTheme({
   palette: {
-    primary: { main: '#ffffff' },
+    // primary: { main: '#ffffff' },
     background: { default: '#f8f9fc' },
-    text: { secondary: 'rgb(0,0,0,0.4)' }
+    text: { secondary: 'rgb(0,0,0,0.4)' },
     //divider: '#addced'
   },
   typography: {
     // allVariants: { color: "#ffffff" },
-    'subtitle1': { fontSize: 17, color: 'rgb(0,0,0,0.8)' },
+    'subtitle1': { fontSize: 17, color: 'rgb(0,0,0,0.5)',  fontWeight: 600, textTransform:'capitalize'  },
     'subtitle2': { color: 'rgb(0,0,0,0.8)' },
     caption: { fontSize: 15, color: 'rgb(0,0,0,0.5)', fontWeight: 600, textTransform: 'uppercase' },
     h1: { fontSize: 80, fontWeight: 'bold' },
--- src/app/pages/Login/Login.jsx
+++ src/app/pages/Login/Login.jsx
@@ -1,5 +1,96 @@
+import { Button, Container, IconButton, InputAdornment, makeStyles, TextField, Typography, useTheme } from "@material-ui/core";
+import { AccountCircle, Lock, Visibility, VisibilityOff } from "@material-ui/icons";
+import { useEffect, useReducer, useState } from "react";
+import logo from "../../../misc/BTB-logo.svg"
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        paddingTop: "60px",
+        height: "100%",
+        display:"flex",
+        flexDirection:"column",
+        '& img': {
+            width: "80vw"
+        }
+    },
+    logo: {
+        paddingBottom:60,
+        flex: "0 0 25%"
+    },
+    loginForm: {
+        display: 'flex',
+        flexDirection:'column',
+        alignItems:'center',
+        flex: "0 0 75%",
+
+        '& .MuiFormControl-root':{
+            margin:15,
+            width: "100%",
+        }
+    }
+}))
+
 const Login = () => {
-    return ( <>fgh</> );
+
+    const theme = useTheme()
+    const classes = useStyles()
+
+    const formReducer = (state, e) => {
+        return { ...state, [e.target.name]: e.target.value }
+    }
+
+    const [form, setForm] = useReducer(formReducer, {
+        id: "",
+        password: ""
+    })
+    const [passVisible, setPassVisible] = useState(false)
+
+    useEffect(() => {
+        console.log(form.password.length)
+    }, [form.password])
+
+    return (
+        <Container maxWidth="lg" className={classes.container}>
+            <div className={classes.logo}>
+                <img src={logo} alt=""></img>
+                <Typography variant="subtitle1">sign in to continue!</Typography>
+            </div>
+            <form className={classes.loginForm}>
+                <TextField
+                    variant="outlined"
+                    label="ID"
+                    name="id"
+                    onChange={setForm}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment>
+                                <AccountCircle />
+                            </InputAdornment>
+                        )
+                    }}
+                />
+                <TextField
+                    variant="outlined"
+                    label="Password"
+                    name="password"
+                    type={!passVisible ? "password": "text"}
+                    onChange={setForm}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment>
+                                {form.password.length
+                                    ? <IconButton onClick={() => { setPassVisible(state => !state) }}>
+                                        {passVisible ? <Visibility /> : <VisibilityOff />}
+                                    </IconButton>
+                                    : <Lock />}
+                            </InputAdornment>
+                        )
+                    }}
+                />
+                <Button variant="contained" color="primary" fullWidth style={{marginTop: 40}}>login</Button>
+            </form>
+        </Container>
+    );
 }
- 
+
 export default Login;
